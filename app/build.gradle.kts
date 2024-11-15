18cd7be727111910c2f3c9acbdf1379412802c03
CaoAnhVu141 <161787991+CaoAnhVu141@users.noreply.github.com>
2024-11-15 11:54:06 +0700

    commit layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.projectofmobile_group8"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.projectofmobile_group8"
