8116d1dce5f7d31c9e21f19bc62b5f0667679b02
phanlongkhanh <phanlongkhanh.tdc2223@gmail.com>
2024-11-22 15:44:07 +0700

    commit

--- app/src/main/java/com/example/projectofmobile_group8/MainActivity.kt
+++ app/src/main/java/com/example/projectofmobile_group8/MainActivity.kt
@@ -1,15 +1,47 @@
 package com.example.projectofmobile_group8
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+private lateinit var editTaiKhoan: EditText
+private lateinit var editPassWord: EditText
+private lateinit var btnDangNhap: Button
+private lateinit var tvDangKy: TextView
+private lateinit var tvForgot: TextView
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.update_address)
+        setContentView(R.layout.activity_login)
+
+        setControl()
+        setEvent()
+    }
+
+    private fun setControl() {
+        editTaiKhoan = findViewById(R.id.editEmail)
+        editPassWord = findViewById(R.id.editPassword)
+        btnDangNhap = findViewById(R.id.btnDangNhap)
+        tvDangKy = findViewById(R.id.tvDangKy)
+        tvForgot = findViewById(R.id.tvForgot)
+    }
+
+    private fun setEvent() {
+        tvDangKy.setOnClickListener {
+            val intent = Intent(this, MainActivity_Register::class.java)
+            startActivity(intent)
+        }
+        tvForgot.setOnClickListener {
+            val intent = Intent(this, MainActivity_Forgot::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/projectofmobile_group8/MainActivity_Forgot.kt
+++ app/src/main/java/com/example/projectofmobile_group8/MainActivity_Forgot.kt
@@ -1,15 +1,34 @@
 package com.example.projectofmobile_group8
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+private lateinit var btnTroLai: Button
+
+
+class MainActivity_Forgot : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_forgot)
+        setControl()
+        setEvent()
+    }
+
+    private fun setControl() {
+        btnTroLai = findViewById(R.id.btnTroLai)
+
+    }
+    private fun setEvent() {
+        btnTroLai.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
