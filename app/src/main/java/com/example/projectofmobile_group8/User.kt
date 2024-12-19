b9a90ada9a6579fd49009338f255bfbd3adb8746
CaoAnhVu141 <161787991+CaoAnhVu141@users.noreply.github.com>
2024-12-19 20:57:04 +0700

    commit file admin user

--- app/src/main/java/com/example/projectofmobile_group8/User.kt
+++ app/src/main/java/com/example/projectofmobile_group8/User.kt
@@ -1,4 +1,11 @@
 package com.example.projectofmobile_group8
-
-class User {
-}
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+@Parcelize
+data class User(
+    val name: String,
+    val username: String,
+    val email: String,
+    val phone: String,
+    var password: String,
+) : Parcelable
